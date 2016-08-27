b3d918570a11cd5c6ba4befbd921d5dcdaae4d2a
breeze <dongyanghongfe@gmail.com>
2016-08-27 10:29:28 +0800

    删除部分无用文件

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-var c= new Array();
-var c = [];
-var y = [1,'wo','ni',['hehe','da']]
-alert(y[3][1])
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
+}
