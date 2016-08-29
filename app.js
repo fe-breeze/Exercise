03bbaef5581f94efcdb775ae0d82e22bff393cab
breeze <dongyanghongfe@gmail.com>
2016-08-29 08:26:38 +0800

    js练习

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-
-function setUserName() {
-    var myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+"use strict"
+let step;
+for (step = 0; step < 5; step++) {
+  // Runs 5 times, with values of step 0 through 4.
+  console.log('Walking east one step'+step);
 }
-if (!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    var storedName = localStorage.getItem('name');
-    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
-}
-myButton.onclick = function() {
-    setUserName();
-}
+alert(step)
